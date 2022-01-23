8acd91a2644588da8616ee5f88ded15ac035a66b
Tituz175 <tobititus01@gmail.com>
2022-01-23 18:20:28 +0100

    header section completed.

---
+++ index.js
@@ -0,0 +1,8 @@
+const inputElement = document.querySelector("#input-container input");
+
+inputElement.addEventListener("onfocus", () => {
+  // inputElement.setAttribute("class", "input-active");
+  // inputElement.setAttribute("placeholder","sola")
+  // console.log(inputElement) 
+});
+

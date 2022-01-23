bc64b587aeaf6c4f5adff29cb807e417efe406c4
Tituz175 <tobititus01@gmail.com>
2022-01-23 19:52:23 +0100

    the structure of the country elements was done - the styling of the country elements was done. - the sync of input value  with the country elements output was 50%   done. - function for country display was written.

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const inputElement = document.querySelector("#input-container input");
+const countryContainer = document.querySelector("#country-container");
 
-inputElement.addEventListener("onfocus", () => {
-  // inputElement.setAttribute("class", "input-active");
-  // inputElement.setAttribute("placeholder","sola")
-  // console.log(inputElement) 
+// inputElement.addEventListener("onfocus", () => {
+//   // inputElement.setAttribute("class", "input-active");
+//   // inputElement.setAttribute("placeholder","sola")
+//   // console.log(inputElement)
+// });
+
+countries.forEach((element) => {
+  console.log(element);
+  let country = document.createElement("div");
+  country.textContent = element.toUpperCase();
+  country.setAttribute("class", "countries");
+  countryContainer.appendChild(country);
 });
 
+let search;
+let country
+inputElement.addEventListener("input", () => {
+  search = inputElement.value;
+  console.log(search);
+  if (search.length >= 1) {
+    countryContainer.innerHTML = "";
+    countries.forEach((element) => {
+      if (element.toLowerCase().includes(search)) {
+        country = document.createElement("div");
+        country.textContent = element.toUpperCase();
+        country.setAttribute("class", "countries");
+        countryContainer.appendChild(country);
+      }
+      
+    });
+  }
+});
+
+countries.forEach((e) => {
+  if (e.toLowerCase().includes("c")) {
+    console.log(e);
+  }
+});
